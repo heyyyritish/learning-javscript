9b2724038e40f24735c6640b8c1d469cfe14a85f
heyyyritish <ritishhh.js@gmail.com>
2024-05-13 15:03:59 +0000

    chapter 2: Variables and typeof

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as new version
+
+// alert(3+3); //will give error as we are using node js, this works on the browser DOM
+
+/**
+ * Code should be readable enough
+ */
+
+let name = 'Ritish'
+let age = 23
+let isMale = true
+
+/**
+ * Data Types available 
+ * 
+ * -> Number
+ *    BigInt for big apps like Stock apps, Fb etc.
+ * -> String
+ * -> Boolean
+ * -> null = stand alone value
+ * -> undefined
+ * -> symbol = unique data type 
+ * -> object
+ */
+
+console.log(typeof name); //string
+console.log(typeof null); //string
+console.log(typeof undefined); //undefined
+
+// console.log(typeof null === typeof undefined);
