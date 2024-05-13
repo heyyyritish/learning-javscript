6efca9e2d08494cf303fee8773b7c5bb104a4489
heyyyritish <ritishhh.js@gmail.com>
2024-05-13 15:31:38 +0000

    type conversion in javascript :: Number Boolean String

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,48 @@
+let age = false
+
+let ageInNumber = Number(age)
+// console.log(ageInNumber);
+
+// console.log(typeof(NaN)); //returns number
+
+/**
+ * '43' -> 43
+ * 43 -> 43
+ * '43a' -> NaN
+ * '' -> 0
+ * 'null' -> NaN
+ * null -> 0'
+ * undefined -> NaN
+ * true -> 1
+ * false -> 0
+ */
+
+let isLoggedIn = NaN
+
+let booleanValue = Boolean(isLoggedIn)
+// console.log(booleanValue);
+
+/**
+ * 1 -> true
+ * 0 -> false
+ * '' -> false
+ * 'false' -> true
+ *  null -> false
+ * undefined -> false
+ * NaN -> false
+ */
+
+let nonStringVariable = true
+
+let stringVariable = String(nonStringVariable)
+console.log(stringVariable);
+console.log(typeof stringVariable)
+
+/**
+ * 23 -> '23'
+ * '33' -> '33'
+ * null -> null && type = string
+ * undefined -> undefined && type = string
+ *  false -> 'false'
+ * true -> 'string'
+ */
