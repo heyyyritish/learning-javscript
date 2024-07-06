b7244dd0af644cb444d294cbbd03f8e3077d4d70
Hey Ritishhh <ritishhh.js@gmail.com>
2024-07-06 19:48:10 +0000

    date in js

---
+++ 01_basics/07_dates_and_time.js
@@ -0,0 +1,15 @@
+/**
+ * Jan 1, 1970 UTC
+ */
+
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+
+console.log(typeof(myDate))
+
+let myArbDate = new Date(2022, 0, 23)
+console.log(myArbDate.toDateString())
+
+
